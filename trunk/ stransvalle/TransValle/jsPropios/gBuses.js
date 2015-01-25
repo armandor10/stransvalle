635010b8bf08c548f30b5dca62b1fedf1999d581
orley333@gmail.com <orley333@gmail.com@9f4523e4-7926-f372-a2bc-2cd775b6bbe2>
2015-01-25 19:00:42 +0000

    Se agrego un nuevo modelo de datos

---
+++ trunk/ stransvalle/TransValle/jsPropios/gBuses.js
@@ -0,0 +1,27 @@
+﻿var gBuses = (function () {
+    var sesion = false;
+
+    var _addHandlers = function () {
+        $("#btnDetallesBus").click(function () {
+            $('#ModalDetallesBus').modal('show');
+        });
+    };
+    var _createElements = function () {
+    };
+    var _VerificarPermisos = function () {
+        return true;
+    };
+    
+    return {
+        init: function () {
+            if (_VerificarPermisos()) {
+                _createElements();
+                _addHandlers();
+            } else window.location.href = "index.html";
+        }
+    };
+}());
+
+$(document).ready(function () {
+    gBuses.init();
+});
