b42dd7a9bca41e5529414d8743185422851d71f7
orley333@gmail.com <orley333@gmail.com@9f4523e4-7926-f372-a2bc-2cd775b6bbe2>
2015-01-28 16:32:15 +0000

    se agrego la base de datos

--- trunk/ stransvalle/Entidades/busesDto.cs
+++ trunk/ stransvalle/Entidades/busesDto.cs
@@ -13,12 +13,19 @@ namespace Entidades
         public string Capacidad { get; set; }
         public Nullable<int> ClaseBus { get; set; }
         public Nullable<int> ClaseServicio { get; set; }
-        public Nullable<System.DateTime> FechaMatricula { get; set; }
+        public DateTime FechaMatricula { get; set; }
         public string Marca { get; set; }
         public string Modelo { get; set; }
         public string NumeroChasis { get; set; }
         public string NumeroMotor { get; set; }
         public string Observaciones { get; set; }
         public string Password { get; set; }
+
+        public string NombreClaseBus { get; set; }
+        public string NombreClaseServicio { get; set; }
+
+        public int DiaMatricula { get; set; }
+        public int MesMatricula { get; set; }
+        public int AñoMatricula { get; set; }
     }
 }
