369a901edd59d58f9602f62a8a4ffed0eda749c1
armandorp10 <armandorp10@gmail.com>
2015-03-18 18:18:48 +0000

    Pruebas unitarias

---
+++  stransvalle/UnitTest/tPlanillaControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+using BLL;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class tPlanillaControl
+    {
+        [TestMethod]
+        public void getRotacionBuses()
+        {
+            mPlanillaControl lg = new mPlanillaControl();
+            List<List<detallesplanillaDTO>> lldP=  lg.getRotacionBuses(1, 4);
+            Assert.IsNotNull(lldP);
+        }
+
+        [TestMethod]
+        public void daysBetweenDates() { 
+            double days=59;
+            double daysReal = ((new DateTime(DateTime.Now.Year, 3, 1)) - (new DateTime(DateTime.Now.Year, 1, 1))).TotalDays;
+            Assert.AreEqual(days, daysReal);
+        }
+    }
+}
