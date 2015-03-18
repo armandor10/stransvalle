6077eaa3fec7889cad8fef2831a5c2a4b1683c3e
armandorp10 <armandorp10@gmail.com>
2015-03-18 18:08:08 +0000

    Nuevas entidades y modificacion de rutasDto

---
+++  stransvalle/Entidades/detallesplanillaDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+  public  class detallesplanillaDTO
+    {      
+      public int id { get; set; }
+      public int idPlanillaControl { get; set; }
+      public string PlacaBus { get; set; }
+      public string Ruta { get; set; }
+
+      public string Vial { get; set; }
+
+    }
+}
---
+++  stransvalle/Entidades/horarioDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+   public class horarioDTO
+    {
+        public int id { get; set; }
+        public Nullable<System.TimeSpan> hora { get; set; }
+        public string time;
+    }
+}
