e39391f5ea6e3e1a4f683e3ed5f0e800e243ccae
armandorp10@gmail.com <armandorp10@gmail.com@9f4523e4-7926-f372-a2bc-2cd775b6bbe2>
2015-03-18 18:18:48 +0000

    Pruebas unitarias

---
+++ trunk/ stransvalle/UnitTest/tHorario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using BLL;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class tHorario
+    {
+        [TestMethod]
+        public void getHorario()
+        {
+            mHorario lg = new mHorario();
+            List<horarioDTO> lHor = lg.get("ruta 12");
+            Assert.IsNotNull(lHor);
+        }
+       
+    }
+}
