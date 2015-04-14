356d2adb35296476e52f6ed4c78e164f11e2bf23
orley333@gmail.com <orley333@gmail.com@9f4523e4-7926-f372-a2bc-2cd775b6bbe2>
2015-04-14 14:27:39 +0000

    Capa de presentacion - Entradas Salidas

---
+++ trunk/ stransvalle/TransValle/Servicios/wsEntradasSalidas.asmx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Entidades;
+using BLL;
+using System.Web.Script.Services;
+
+namespace TransValle.Servicios
+{
+    /// <summary>
+    /// Descripción breve de wsEntradasSalidas
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    [System.Web.Script.Services.ScriptService]
+    public class wsEntradasSalidas : System.Web.Services.WebService
+    {
+        [WebMethod]
+        [ScriptMethod(UseHttpGet = true, ResponseFormat = ResponseFormat.Json)]
+        public List<entradassalidasDTO> GetUltimas()
+        {
+            mEntradasSalidas objES = new mEntradasSalidas();
+            return objES.Getultimas();
+        }
+        [WebMethod]
+        [ScriptMethod(UseHttpGet = true, ResponseFormat = ResponseFormat.Json)]
+        public objRes Insert(entradassalidasDTO Reg)
+        {
+            mEntradasSalidas objES = new mEntradasSalidas();
+            return objES.Insert(Reg);
+        }
+    }
+}
