8c6c7de0a8f0813eac7d4916ebdae5d36f43f07f
armandorp10@gmail.com <armandorp10@gmail.com@9f4523e4-7926-f372-a2bc-2cd775b6bbe2>
2015-05-14 19:08:39 +0000

    - nuevos atributos a buses - nueva entidad documentosbus

---
+++ trunk/ stransvalle/Entidades/documentosbusDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class documentosbusDTO
+    {
+        public int id { get; set; }
+        public Nullable<int> documento { get; set; }
+        public string placa { get; set; }
+        public string Numero { get; set; }
+        public Nullable<System.DateTime> fechaExpedicion { get; set; }
+        public Nullable<System.DateTime> fechaExpiracion { get; set; }
+    }
+}
